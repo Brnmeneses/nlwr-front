4d33ef7ed3b7a02226fea6987088c6292cbe22ec
Bruno Meneses <brnmeneses@gmail.com>
2022-05-06 23:00:04 -0300

    integration axios

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -43,6 +43,10 @@ export function WidgetForm() {
     setFeedbackType(null);
   }
 
+  function handleSentFeedback() {
+    setFeedbackSent(true);
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       {feedbackSent ? (
@@ -58,7 +62,7 @@ export function WidgetForm() {
             <FeedbackContentStep
               feedbackType={feedbackType}
               onFeedbackRestartRequested={handleRestartFeedback}
-              onFeedbackSent={() => setFeedbackSent(true)}
+              onFeedbackSent={handleSentFeedback}
             />
           )}
         </>
